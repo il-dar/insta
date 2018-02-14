a53a9b9579c180f23b67cbf2f78014603dcf42c5
il-dar <themagnacarta@protonmail.com>
2018-02-14 12:29:35 -0500

    three

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  validates :image, presence: true
+  has_attached_file :image
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+
 end
