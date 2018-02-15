e2592a7cc9b8692b1bc87a353e0f63b99001f1ae
il-dar <themagnacarta@protonmail.com>
2018-02-14 21:05:50 -0500

    Bootstrap added, edit, delete, show added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 
 
 def index
-    @posts = Post.all 
+    @posts = Post.all
 end
 
 def new
@@ -15,7 +15,25 @@ def create
   redirect_to posts_path
 end
 
+def show
+  @post = Post.find(params[:id])
+end
 
+def edit
+  @post = Post.find(params[:id])
+end
+
+def update
+  @post = Post.find(params[:id])
+  @post.update(post_params)
+  redirect_to(post_path(@post))
+end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect_to posts_path
+end
 
 private
 
